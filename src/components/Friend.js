5426855eb8b9d3dcd33bef525ab07ec5894fc0d4
Blaine Schmeisser <blainesch@gmail.com>
2018-01-09 19:33:37 -0800

    Add ability to remove a friend's public key.

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -2,10 +2,14 @@ import React, { Component, PropTypes } from 'react';
 import { css, StyleSheet } from 'aphrodite';
 import { Link } from 'react-router';
 import { colours } from 'utils/constants';
+import { remote } from 'electron';
+
+const { dialog } = remote;
 
 const componentStyles = StyleSheet.create({
   friend: {
     display: 'flex',
+    flexWrap: 'wrap',
     alignItems: 'center',
     padding: 15,
     borderBottom: `1px solid ${colours.white}`,
@@ -20,20 +24,42 @@ const componentStyles = StyleSheet.create({
   name: {
     fontSize: 14,
     fontWeight: 600,
-    flexGrow: 1,
+    width: '93%',
+  },
+  delete: {
+    width: '7%',
   },
   email: {
     fontSize: 10,
+    width: '100%',
   },
 });
 
 export default class Friend extends Component {
 
   static propTypes = {
+    onRemove: PropTypes.func,
     friend: PropTypes.shape({
+      id: PropTypes.string,
       name: PropTypes.string,
       email: PropTypes.string,
     }),
+  }
+
+  handleDeleteClick = (e) => {
+    const { friend, onRemove } = this.props;
+    const { id, name, email } = friend;
+    const choice = dialog.showMessageBox({
+      type: 'question',
+      buttons: ['Yes', 'No'],
+      defaultId: 1,
+      title: 'Confirm',
+      message: `Are you sure you wish to delete the key for "${name} <${email}>"?`,
+    });
+    if (choice === 0) {
+      onRemove(id);
+    }
+    e.preventDefault();
   }
 
   render() {
@@ -45,6 +71,7 @@ export default class Friend extends Component {
         to={`/dashboard/encrypt/${friend.id}`}
       >
         <span className={css(componentStyles.name)}>{friend.name}</span>
+        <span onClick={this.handleDeleteClick} className={css(componentStyles.delete)}>x</span>
         <span className={css(componentStyles.email)}>{friend.email}</span>
       </Link>
     );
